48877fe8e9a9cf6131653d5ec894eae454acd9be
KAVlex <kavlex90@gmail.com>
2017-03-18 20:13:19 +0300

    Задание 3, создал два списка покемонов lost и found

--- main.js
+++ main.js
@@ -20,3 +20,13 @@ class PokemonList extends Array{
     super.push(new Pokemon(name, level));
   }
 }
+
+var squirtle = new Pokemon('Сквиртл', 1);
+var wartortle = new Pokemon('Вартортл', 2);
+
+var lost = new PokemonList(squirtle, wartortle);
+var found = new PokemonList(
+    new Pokemon('Чармандер', 1),
+    new Pokemon('Чармелеон', 2),
+    new Pokemon('Чаризард', 3)
+  );
